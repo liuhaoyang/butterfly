bc3f68ade58007870bbb785650c4eff7185b67a8
Lemon <liuhaoyang1221@hotmail.com>
2017-09-28 22:23:36 +0800

    update ISpanContext

--- core/src/Butterfly.APM.Core.OpenTracing/ISpanContext.cs
+++ core/src/Butterfly.APM.Core.OpenTracing/ISpanContext.cs
@@ -1,7 +1,15 @@
-﻿namespace Butterfly.APM.Core.OpenTracing
+﻿using System;
+
+namespace Butterfly.APM.Core.OpenTracing
 {
     public interface ISpanContext
     {
+        string TraceId { get; }
+
+        string SpanId { get; }
+
+        bool Sampled { get; }
+
         ReadOnlyBaggage GetBaggage();
     }
 }
