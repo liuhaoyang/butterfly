b241617fea220f5deeb61efdcedc053260c89748
Lemon <liuhaoyang1221@hotmail.com>
2017-10-17 17:46:06 +0800

    Add core project

---
+++ src/Butterfly.APM.Core/Module/IModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AspectCore.Injector;
+
+namespace Butterfly.APM.Core.Module
+{
+    public interface IModule
+    {
+        string Name { get; }
+
+        void Configure(IServiceContainer services);
+
+        void Initialize(IServiceResolver resolver);
+    }
+}
---
+++ src/Butterfly.APM.Core/Module/IModuleFinder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Butterfly.APM.Core.Module
+{
+    public interface IModuleFinder
+    {
+        IEnumerable<IModule> Find();
+    }
+}
