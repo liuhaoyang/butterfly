7df2c4bf47e35807052a751282f6fc55257dca49
lemon <liuhaoyang1221@hotmail.com>
2017-12-16 20:00:26 +0800

    refactor

---
+++ src/Butterfly.Core/Collector/IResetEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace Butterfly.Core
+{
+    public interface IResetEvent : IDisposable
+    {
+        bool IsSet { get; }
+
+        void Reset();
+
+        void Set();
+
+        void Wait(CancellationToken cancellationToken);
+
+        void Wait(TimeSpan timeout, CancellationToken cancellationToken);
+    }
+}
