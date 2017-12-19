26b33509a37234a01509a8563bdddfc30282a3ed
Lemon <liuhaoyang1221@hotmail.com>
2017-12-19 10:16:38 +0800

    add SpanResponse

---
+++ src/Butterfly.Server/Common/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Butterfly.Protocol;
+using Butterfly.Server.Models;
+
+namespace Butterfly.Server.Common
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Span, SpanResponse>().ReverseMap();
+        }
+    }
+}
