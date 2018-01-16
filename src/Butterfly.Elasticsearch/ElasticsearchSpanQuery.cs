6df6b09ce6c658909428280099051b7977a934c7
lemon <liuhaoyang1221@hotmail.com>
2018-01-16 23:11:15 +0800

    refactor

--- src/Butterfly.Elasticsearch/ElasticsearchSpanQuery.cs
+++ src/Butterfly.Elasticsearch/ElasticsearchSpanQuery.cs
@@ -36,21 +36,9 @@ namespace Butterfly.Elasticsearch
 
             var query = BuildTracesQuery(traceQuery);
 
-            var request = new SearchRequest(index)
-            {
-                From = (traceQuery.CurrentPageNumber - 1) * traceQuery.PageSize,
-                Size = traceQuery.PageSize,
-                Query = query
-            };
+            var searchQuery = _elasticClient.SearchAsync<Span>(search => search.Index(index).Query(query).From((traceQuery.CurrentPageNumber - 1) * traceQuery.PageSize).Size(traceQuery.PageSize).Sort(sort => sort.Descending(x => x.StartTimestamp)));
 
-            var searchQuery = _elasticClient.SearchAsync<Span>(request);
-
-            var countRequest = new CountRequest(index)
-            {
-                Query = query
-            };
-
-            var totalMemberCountQuery = _elasticClient.CountAsync<Span>(countRequest);
+            var totalMemberCountQuery = _elasticClient.CountAsync<Span>(c => c.Index(index).Query(query));
 
             await Task.WhenAll(searchQuery, totalMemberCountQuery);
 
@@ -62,9 +50,9 @@ namespace Butterfly.Elasticsearch
             {
                 CurrentPageNumber = traceQuery.CurrentPageNumber,
                 PageSize = traceQuery.PageSize,
-                TotalMemberCount = (int)totalMemberCount,
-                TotalPageCount = (int)Math.Ceiling((double)totalMemberCount / (double)traceQuery.PageSize),
-                Data = spans.Documents.GroupBy(x => x.TraceId).Select(x => new Trace { TraceId = x.Key, Spans = x.ToList() })
+                TotalMemberCount = (int) totalMemberCount,
+                TotalPageCount = (int) Math.Ceiling((double) totalMemberCount / (double) traceQuery.PageSize),
+                Data = spans.Documents.GroupBy(x => x.TraceId).Select(x => new Trace {TraceId = x.Key, Spans = x.ToList()})
             };
         }
 
@@ -78,27 +66,53 @@ namespace Butterfly.Elasticsearch
             throw new System.NotImplementedException();
         }
 
-        private QueryContainer BuildTracesQuery(TraceQuery traceQuery)
+        private Func<QueryContainerDescriptor<Span>, QueryContainer> BuildTracesQuery(TraceQuery traceQuery)
         {
-            var query = new QueryContainerDescriptor<Span>();
-            return query.Bool(b => b.Must(BuildMustQuery(traceQuery)));
+            return query => query.Bool(b => b.Filter(BuildMustQuery(traceQuery)));
         }
 
         private IEnumerable<Func<QueryContainerDescriptor<Span>, QueryContainer>> BuildMustQuery(TraceQuery traceQuery)
         {
             if (traceQuery.StartTimestamp != null)
             {
-                yield return new Func<QueryContainerDescriptor<Span>, QueryContainer>(q => q.DateRange(d => d.Field(x => x.StartTimestamp).GreaterThanOrEquals(traceQuery.StartTimestamp.Value.DateTime)));
+                yield return q => q.DateRange(d => d.Field(x => x.StartTimestamp).GreaterThanOrEquals(traceQuery.StartTimestamp.Value.DateTime));
             }
+
             if (traceQuery.FinishTimestamp != null)
             {
-                yield return new Func<QueryContainerDescriptor<Span>, QueryContainer>(q => q.DateRange(d => d.Field(x => x.FinishTimestamp).LessThanOrEquals(traceQuery.FinishTimestamp.Value.DateTime)));
+                yield return q => q.DateRange(d => d.Field(x => x.FinishTimestamp).LessThanOrEquals(traceQuery.FinishTimestamp.Value.DateTime));
+            }
+
+            foreach (var queryTag in BuildQueryTags(traceQuery))
+            {
+                yield return q => q.Bool(b => b.Filter(f => f.Term(new Field("tags.key"), queryTag.Key), f => f.Term(new Field("tags.value"), queryTag.Value)));
             }
         }
 
         private Indices BuildIndices(DateTimeOffset? startTimestamp, DateTimeOffset? finishTimestamp)
         {
-            return null;
+            return Indices.Index(_indexFactory.CreateIndex());
+        }
+
+        private IEnumerable<Tag> BuildQueryTags(TraceQuery traceQuery)
+        {
+            if (!string.IsNullOrEmpty(traceQuery.ServiceName))
+            {
+                yield return new Tag {Key = QueryConstants.Service, Value = traceQuery.ServiceName};
+            }
+
+            if (!string.IsNullOrEmpty(traceQuery.Tags))
+            {
+                var tags = traceQuery.Tags.Split('|');
+                foreach (var tag in tags)
+                {
+                    var pair = tag.Split('=');
+                    if (pair.Length == 2)
+                    {
+                        yield return new Tag {Key = pair[0], Value = pair[1]};
+                    }
+                }
+            }
         }
     }
 }
