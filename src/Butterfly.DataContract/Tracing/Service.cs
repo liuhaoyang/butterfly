566ff5f28a2b591c1c175da9ee974245eeae8206
Lemon <liuhaoyang1221@hotmail.com>
2018-01-21 18:17:27 +0800

    refactor

---
+++ src/Butterfly.DataContract/Tracing/Service.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Butterfly.DataContract.Tracing
+{
+    public class Service
+    {
+        public string Name { get; set; }
+
+        public string SpanId { get; set; }
+
+        public DateTimeOffset StartTimestamp { get; set; }
+    }
+}
