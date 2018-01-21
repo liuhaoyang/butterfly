5934fe4bef7c83955316e65ac7e2efd6d9439a4d
Lemon <liuhaoyang1221@hotmail.com>
2018-01-21 18:17:01 +0800

    Add IServiceStorage

---
+++ src/Butterfly.Core/Storage/IServiceStorage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Butterfly.DataContract.Tracing;
+
+namespace Butterfly.Storage
+{
+    public interface IServiceStorage
+    {
+        Task StoreServiceAsync(IEnumerable<Service> services, CancellationToken cancellationToken);
+    }
+}
--- src/Butterfly.Core/Storage/Query/TraceQuery.cs
+++ src/Butterfly.Core/Storage/Query/TraceQuery.cs
@@ -2,20 +2,16 @@
 
 namespace Butterfly.Storage.Query
 {
-    public class TraceQuery
+    public class TraceQuery : TimeRangeQuery
     {
         public string ServiceName { get; set; }
-
-        public DateTimeOffset? StartTimestamp { get; set; }
-
-        public DateTimeOffset? FinishTimestamp { get; set; }
 
         public int? MinDuration { get; set; }
 
         public int? MaxDuration { get; set; }
-        
+
         public int Limit { get; set; }
-        
+
         public string Tags { get; set; }
     }
 }
